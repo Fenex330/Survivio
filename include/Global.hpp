22ce9c6cf05bab061b334c9f4004fdc5e9ebb3b0
Ilya Ballet <anonimus33@protonmail.com>
2020-11-12 00:00:36 +0200

    added PI constant

--- include/Global.hpp
+++ include/Global.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <cmath>
+
 /* Store all global defines and constants here */
 
 namespace surviv
 {
+    constexpr long double PI = std::atan (1) * 4;
+
     const unsigned short view_dim_X = 800;
     const unsigned short view_dim_Y = 800;
     const unsigned int default_player_speed = 10;
