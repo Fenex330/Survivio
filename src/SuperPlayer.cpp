28fc2af14e3c8cb71b102e2073631ed09b43deaa
Ilya Ballet <anonimus33@protonmail.com>
2020-11-11 22:42:06 +0200

    deprecated arrows use for movement (will add customizable keybinds in the future)

--- src/SuperPlayer.cpp
+++ src/SuperPlayer.cpp
@@ -11,22 +11,22 @@ void SuperPlayer::move (Map &map)
 {
     int mul = surviv::default_player_speed; //multiply by speed coefficients
 
-    if (sf::Keyboard::isKeyPressed (sf::Keyboard::Right) || sf::Keyboard::isKeyPressed (sf::Keyboard::D))
+    if (sf::Keyboard::isKeyPressed (sf::Keyboard::D))
     {
         sprite.move (1 * mul, 0 * mul);
         map.mainView.move (1 * mul, 0 * mul);
     }
-    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Left) || sf::Keyboard::isKeyPressed (sf::Keyboard::A))
+    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::A))
     {
         sprite.move (-1 * mul, 0 * mul);
         map.mainView.move (-1 * mul, 0 * mul);
     }
-    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Up) || sf::Keyboard::isKeyPressed (sf::Keyboard::W))
+    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::W))
     {
         sprite.move (0 * mul, -1 * mul);
         map.mainView.move (0 * mul, -1 * mul);
     }
-    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::Down) || sf::Keyboard::isKeyPressed (sf::Keyboard::S))
+    else if (sf::Keyboard::isKeyPressed (sf::Keyboard::S))
     {
         sprite.move (0 * mul, 1 * mul);
         map.mainView.move (0 * mul, 1 * mul);
